7798349836332c617770db875b3ab88d3db70bd6
imlidya <imlidya18@gmail.com>
2021-08-20 21:21:46 +0700

    user_info

--- github/src/containers/UserPage.js
+++ github/src/containers/UserPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import UserInfo from '../components/UserInfo'
 
 function userPage() {   
     return (
         <div>
             <NavBar></NavBar>
+            <UserInfo></UserInfo>
         </div>
     )
 }
