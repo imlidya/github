1775cbdcdd060d7a8ac16a701d110a6acd36c655
imlidya <imlidya18@gmail.com>
2021-08-20 21:34:35 +0700

    tabs

---
+++ github/src/components/Tabs.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../styles/Tabs.css'
+
+const Tabs = () => (
+    <ul className="nav nav-tabs">
+        <li className="nav-item">
+            <p className="nav-link mb-0 active">Repositories</p>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link mb-0" href="#">Packages</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link mb-0" href="#">People</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link mb-0" href="#">Projects</a>
+        </li>
+    </ul>
+)
+export default Tabs
--- github/src/containers/UserPage.js
+++ github/src/containers/UserPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import UserInfo from '../components/UserInfo'
+import Tabs from '../components/Tabs'
 
 function userPage() {   
     return (
         <div>
             <NavBar></NavBar>
             <UserInfo></UserInfo>
+            <Tabs></Tabs>
         </div>
     )
 }
